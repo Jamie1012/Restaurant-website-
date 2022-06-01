6902e1aa3217df3bba25b3d84b643f7c602229a1
Jasmine Anand <jasminnie1012@gmail.com>
2022-06-01 16:09:25 +0530

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Restaurants from "./component/Basics/Restaurants";
+
+function App() {
+  return (
+  <>
+   <Restaurants />
+  </>
+  );
+}
+
+export default App;
