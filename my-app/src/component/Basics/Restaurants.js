95a407111b305d42dfa4f9374d51cd39503b3028
Jasmine Anand <jasminnie1012@gmail.com>
2022-06-01 16:56:47 +0530

    img added

--- my-app/src/component/Basics/Restaurants.js
+++ my-app/src/component/Basics/Restaurants.js
@@ -9,7 +9,14 @@ const Restaurants = () => {
          <div className="card">
               <div className="card-body">
                     <span className="card-number card-circle subtle ">1</span>
+                    <span className="card-author subtle" > Breakfast</span>
+                    <h2 className="card-title"> Maggie</h2>
+                    <span className="card-description subtle">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptate maxime, eveniet ullam quibusdam alias.
+                    </span>
+                    <div className="card-read">Read</div>
               </div>
+              <img src={image} alt="images" className="card-media" />
          </div>
     </div>
     </>
