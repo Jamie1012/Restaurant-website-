25bbb1ec9f5f3e73844ed46ecbee39cea8cc12b0
Jasmine Anand <jasminnie1012@gmail.com>
2022-06-01 22:49:19 +0530

    nav added

--- my-app/src/component/Basics/Restaurants.js
+++ my-app/src/component/Basics/Restaurants.js
@@ -9,9 +9,29 @@ const Restaurants = () => {
 
   // To manage data
   const [menuData, setMenuData] = useState(Menu);
-  return <>
+
+   const filtrtItem = (category) => {
+   const updatedList = Menu.filter((curElem) =>{
+     return curElem.category === category;
+   });
+    setMenuData(updatedList);
+   };
+  return ( 
+  <>
+
+   <nav className="navbar">
+     <div className="btn-group"> 
+         <button className="btn-group__item" onClick={() =>filtrtItem("breakfast")}> Breakfast</button>
+         <button className="btn-group__item" onClick={() =>filtrtItem("lunch")}> lunch</button>
+         <button className="btn-group__item" onClick={() =>filtrtItem("evening")}> Evening</button>
+         <button className="btn-group__item" onClick={() =>filtrtItem("dinner")}> Dinner</button>
+         <button className="btn-group__item" onClick={() =>setMenuData(Menu)}> All</button>
+     </div>
+
+   </nav>
   <MenuCard menuData={menuData} />
-  </>;
+  </>
+  );
 } 
 
 export default Restaurants;
