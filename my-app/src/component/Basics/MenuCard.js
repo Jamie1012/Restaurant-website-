29bf8f3ed262829379a5289a15b86a025d5ebd4d
Jasmine Anand <jasminnie1012@gmail.com>
2022-06-01 22:30:43 +0530

    api fixed

--- my-app/src/component/Basics/MenuCard.js
+++ my-app/src/component/Basics/MenuCard.js
@@ -9,21 +9,32 @@ const MenuCard = ( {menuData} ) => {
    console.log(menuData);
     return (
       <>
-      <div className="card-container">
-          <div className="card">
-                <div className="card-body">
-                      <span className="card-number card-circle subtle ">1</span>
-                      <span className="card-author subtle" > Breakfast</span>
-                      <h2 className="card-title"> Maggie</h2>
-                      <span className="card-description subtle">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptate maxime, eveniet ullam quibusdam alias.
-                      </span>
-                      <div className="card-read">Read</div>
-                </div>
-                {/* <img src={image} alt="images" className="card-media" /> */}
-            </div>
-      </div>
-    </>
+      <section className='main-card--cointainer'>
+      {menuData.map((curElem) => {
+           return (
+            <>
+           <div className="card-container" key={curElem.id} >
+           <div className="card">
+                 <div className="card-body">
+                       <span className="card-number card-circle subtle ">{curElem.id}</span>
+                       <span className="card-author subtle" > {curElem.category}</span>
+                       <h2 className="card-title"> {curElem.name}</h2>
+                       <span className="card-description subtle">
+                       {curElem.description}
+                       </span>
+                       <div className="card-read">Read</div>
+                 </div>
+                 <img src={curElem.image} alt="images" className="card-media" />
+             </div>
+       </div>
+     </>
+        )
+      }
+        
+      )}
+      </section>
+    
+     </>
     );
   };
    
